9d273dab6ff0dfb33aa033e490e364b578ffe0e6
hejazinasr <mhejazinasr@gmail.com>
2023-03-29 17:24:25 -0400

    First CCommit

---
+++ server/models/takeasurvey.js
@@ -0,0 +1,81 @@
+let mongoose = require('mongoose');
+
+// create a model class
+let takeasurvey = mongoose.Schema({
+<<<<<<< HEAD
+   
+=======
+>>>>>>> 541e205932f39f64c4261dce5bba8dab26c0a162
+   username:String,
+   displayName:String,
+   surveyId:String,
+   surveyOwner:String,
+    surveyName:String, 
+    surveyDescription:String,
+    createdDate:String,  
+    startDate:String,
+    endDate:String,
+   modifiedDate:String,
+    url: String,
+    Questionnaire_type:String,
+    Question1:String,
+    Question2:String,
+    Question3:String,
+    Question4:String,
+<<<<<<< HEAD
+    Question5:String,
+=======
+>>>>>>> 541e205932f39f64c4261dce5bba8dab26c0a162
+    Question1Answer:String,
+    Question2Answer:String,
+    Question3Answer:String, 
+    Question4Answer:String,
+    Question5Answer:String,
+    Questionlength:Number,
+    Questionlengthtf:Number,
+    Question1AnswerLength:Number,    
+    Question2AnswerLength:Number,
+    Question3AnswerLength:Number,
+    Question4AnswerLength:Number,
+    Question5AnswerLength:Number,
+    Question1Answer1:String,
+    Question1Answer2:String,
+    Question1Answer3:String,
+    Question1Answer4:String,
+    Question1Answer5:String,
+    Question2Answer1:String,
+    Question2Answer2:String,
+    Question2Answer3:String,
+    Question2Answer4:String,
+    Question2Answer5:String,
+    Question3Answer1:String,
+    Question3Answer2:String,
+    Question3Answer3:String,
+    Question3Answer4:String,
+    Question3Answer5:String,
+    Question4Answer1:String,
+    Question4Answer2:String,
+    Question4Answer3:String,
+    Question4Answer4:String,
+    Question4Answer5:String,
+    Question5Answer1:String,
+    Question5Answer2:String,
+    Question5Answer3:String,
+    Question5Answer4:String,
+    Question5Answer5:String,
+
+    questions:[	
+        {
+           questionNumber:Number,
+           questionDetail:String,
+           questionFormat: String
+             
+        } 
+     ]
+ 
+},
+{
+  collection: "takeasurvey"
+});
+
+module.exports = mongoose.model('takeasurvey', takeasurvey);
